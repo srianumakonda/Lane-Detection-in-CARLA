36206114e467127aee1736efd2066a89b7ec643d
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-01-14 11:17:25 -0500

    Update model.py

--- model.py
+++ model.py
@@ -3,9 +3,6 @@
 
 import numpy as np 
 import os
-import skimage.io as io
-import skimage.transform as trans
-import numpy as np
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
@@ -14,8 +11,6 @@ from keras import backend as keras
 from keras import backend as K
 import tensorflow as tf
 import matplotlib.pyplot as plt
-# from tensorflow_addons.utils.keras_utils import LossFunctionWrapper
-# from tensorflow_addons.utils.types import FloatTensorLike, TensorLike
 from typeguard import typechecked
 import tensorflow_addons as tfa
 
@@ -30,28 +25,6 @@ class UNet_Model:
         self.loaded_model = load_model(filepath=filepath)
         self.model_loaded = True
         return self.loaded_model
-
-    # def binary_focal_loss(self, gamma=2.0, alpha=0.25):
-    #     def focal_loss(y_true, y_pred):
-    #         epsilon = K.epsilon()
-    #         y_pred = K.clip(y_pred, epsilon, 1.0-epsilon)
-    #         p_t = tf.where(K.equal(y_true, 1), y_pred, 1-y_pred)
-    #         alpha_factor = K.ones_like(y_true)*alpha
-    #         alpha_t = tf.where(K.equal(y_true, 1), alpha_factor, 1-alpha_factor)
-    #         cross_entropy = -K.log(p_t)
-    #         weight = alpha_t * K.pow((1-p_t), gamma)
-    #         loss = weight * cross_entropy
-    #         loss = K.sum(loss, axis=1)
-    #         return loss
-    #     return focal_loss    
-
-    # def focal_loss(self, gamma=2., alpha=.25):
-    #     def focal_loss_fixed(y_true, y_pred):
-    #         pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-    #         pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-    #         return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
-    #     return focal_loss_fixed
-
 
     def unet(self, input_size, loss, metrics):
         inputs = Input(input_size)
@@ -74,14 +47,14 @@ class UNet_Model:
         conv4 = BatchNormalization()(conv4)
         conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
         conv4 = BatchNormalization()(conv4)
-        drop4 = Dropout(0.9)(conv4)
+        drop4 = Dropout(0.5)(conv4)
         pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
         conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
         conv5 = BatchNormalization()(conv5)
         conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
         conv5 = BatchNormalization()(conv5)
-        drop5 = Dropout(0.9)(conv5)
+        drop5 = Dropout(0.5)(conv5)
 
         up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
         merge6 = concatenate([drop4,up6], axis = 3)
@@ -110,13 +83,11 @@ class UNet_Model:
         conv9 = BatchNormalization()(conv9)
         conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
         conv9 = BatchNormalization()(conv9)
-        conv9 = Conv2D(2, 3, activation = 'sigmoid', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+        conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
         conv9 = BatchNormalization()(conv9)
         conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
         
         self.model = Model(inputs = inputs, outputs = conv10)
-
-        # self.model.compile(optimizer=Adam(lr=1e-5), loss=tfa.losses.SigmoidFocalCrossEntropy(), metrics =['accuracy'])
         self.model.compile(optimizer=Adam(lr=1e-2), loss=loss, metrics =[metrics])
         return self.model
 
@@ -124,17 +95,22 @@ class UNet_Model:
         return self.model.summary()
         
     def train_model(self, filepath, X_train, y_train, X_val, y_val, epochs, display_callback):
-        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
         model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=filepath,
                                                                         save_weights_only=True,
-                                                                        monitor='val_dice',
+                                                                        monitor='val_loss',
                                                                         mode='max',
                                                                         save_best_only=True)
+        reducelr = ReduceLROnPlateau(monitor="val_loss",
+                                     factor=0.2,
+                                     patience=3,
+                                     min_lr=1e-5,
+                                     verbose=1)
+                                                    
             
         if self.model_loaded:
-            self.loaded_model.fit(x=X_train, y=y_train, validation_data=(X_val,y_val), steps_per_epoch=14610//32, epochs=epochs, callbacks=[callback, model_checkpoint_callback, display_callback], verbose=1)      
+            self.loaded_model.fit(x=X_train, y=y_train, validation_data=(X_val,y_val), steps_per_epoch=14610//32, epochs=epochs, callbacks=[model_checkpoint_callback, reducelr, display_callback], verbose=1)      
         else:
-            self.model.fit(x=X_train, y=y_train, validation_data=(X_val,y_val), steps_per_epoch=14610//32, epochs=epochs, callbacks=[callback, model_checkpoint_callback, display_callback], verbose=1)           
+            self.model.fit(x=X_train, y=y_train, validation_data=(X_val,y_val), steps_per_epoch=14610//32, epochs=epochs, callbacks=[model_checkpoint_callback, reducelr, display_callback], verbose=1)           
 
     def test_predict(self, X_test, y_test, idx, model_filepath=None):
         if self.model_loaded:
