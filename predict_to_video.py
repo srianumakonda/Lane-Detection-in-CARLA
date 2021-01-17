ab0a5ed5c324536be14e391119ca832cb0e6ef11
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-01-16 20:44:37 -0500

    Update predict_to_video.py

--- predict_to_video.py
+++ predict_to_video.py
@@ -7,6 +7,14 @@ from keras.models import *
 from keras import backend as K
 import matplotlib.pyplot as plt
 
+config = tf.compat.v1.ConfigProto(gpu_options = 
+                         tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8)
+# device_count = {'GPU': 1}
+)
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(session)
+
 if __name__ == "__main__":
 
     loaded_model = load_model("final_model.tf",
@@ -14,9 +22,10 @@ if __name__ == "__main__":
 
     img_list = []
     pred_list = []
+    filepath = "carla-dataset\\train"
 
-    for filename in os.listdir("img/train"):
-        img = cv2.imread(filename)
+    for filename in os.listdir(filepath):
+        img = cv2.imread(os.path.join(filepath,filename))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = cv2.resize(img,(128,128))
         img_list.append(img)
