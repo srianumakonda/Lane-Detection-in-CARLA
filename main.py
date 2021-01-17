9dd2635806f2c8ff7ace95d471c97e78cf3b46fc
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-01-16 20:30:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from preprocess_data import FixData
 from visualize_data import visualize_img
 from preprocess_data import SplitData
 from model import UNet_Model
+import loss
 
 import numpy as np
 import shutil
@@ -9,79 +10,61 @@ import os
 import tensorflow as tf
 from keras import backend as K
 import random
+import matplotlib
 import matplotlib.pyplot as plt
 
-filepath = "carla-dataset" #make sure you specify the directory of the dataset here
-fix_data = FixData(filepath)
-fix_data.rename_folders()
-fix_data.rename_sort_data()
-fix_data.create_val_set(0.2)
+if __name__ == "__main__":
 
-visualize_img(filepath, 2)
-fig = plt.figure(figsize = (20, 10)) 
-plt.bar(["Train", "Test"], [len(os.listdir(os.path.join(filepath, "train/train"))), len(os.listdir(os.path.join(filepath, "test")))],
-        color="maroon")
-matplotlib.rc('xtick', labelsize=20) 
-matplotlib.rc('ytick', labelsize=20) 
+    filepath = "carla-dataset" #make sure you specify the directory of the dataset here
+    fix_data = FixData(filepath)
+    fix_data.rename_folders()
+    fix_data.rename_sort_data()
+    fix_data.create_val_set(0.2)
 
-split_data = SplitData(filepath)
-split_data.resize_img(128,0,30)
-split_data.print_img_pair(0)
-X_train, y_train, X_val, y_val, X_test, y_test = split_data.data()
-print(split_data.get_shapes())
+    visualize_img(filepath, 2)
+    fig = plt.figure(figsize = (20, 10)) 
+    plt.bar(["Train", "Test"], [len(os.listdir(os.path.join(filepath, "train/train"))), len(os.listdir(os.path.join(filepath, "test")))],
+            color="maroon")
+    matplotlib.rc('xtick', labelsize=20) 
+    matplotlib.rc('ytick', labelsize=20) 
 
-# Credit to https://github.com/nabsabraham/focal-tversky-unet/blob/master/losses.py for the loss functions
-
-smooth=1
-def tversky(y_true, y_pred):
-    y_true_pos = K.flatten(y_true)
-    y_pred_pos = K.flatten(y_pred)
-    true_pos = K.sum(y_true_pos * y_pred_pos)
-    false_neg = K.sum(y_true_pos * (1-y_pred_pos))
-    false_pos = K.sum((1-y_true_pos)*y_pred_pos)
-    alpha = 0.7
-    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
-def focal_tversky(y_true,y_pred):
-    pt_1 = tversky(y_true, y_pred)
-    gamma = 0.75
-    return K.pow((1-pt_1), gamma)
-def dice(y_true, y_pred):
-    smooth = 1.
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return score
-
-model = UNet_Model()
-train_model = model.unet(input_size=(128,128,1),loss=focal_tversky,metrics=dice)
-# model.model_summary()
+    split_data = SplitData(filepath)
+    split_data.resize_img(128,0,30)
+    split_data.print_img_pair(0)
+    X_train, y_train, X_val, y_val, X_test, y_test = split_data.data()
+    print(split_data.get_shapes())
 
 
-class DisplayCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs=None):
-    choose_rand = random.randint(0,int(np.array(X_test).ravel().shape[0])//128//128)
-    prediction = np.array(X_test[choose_rand]).reshape(1,128,128,1)
-    plt.figure(figsize=(15, 15))
 
-    plt.subplot(3, 3, 1)
-    plt.imshow(np.squeeze(X_test[choose_rand]),cmap="gray")
-    plt.title("Image")
+    model = UNet_Model()
+    train_model = model.unet(input_size=(128,128,1),loss=loss.focal_tversky,metrics=loss.dice)
+    # model.model_summary()
 
-    plt.subplot(3, 3, 2)
-    plt.imshow(np.squeeze(y_test[choose_rand]),cmap="gray")
-    plt.title("Mask")
 
-    plt.subplot(3, 3, 3)
-    plt.imshow(np.squeeze(train_model.predict(prediction)),cmap="gray")
-    plt.title("Predicted Mask")
+    class DisplayCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs=None):
+            choose_rand = random.randint(0,int(np.array(X_test).ravel().shape[0])//128//128)
+            prediction = np.array(X_test[choose_rand]).reshape(1,128,128,1)
+            plt.figure(figsize=(15, 15))
 
-    plt.axis('off')
+            plt.subplot(3, 3, 1)
+            plt.imshow(np.squeeze(X_test[choose_rand]),cmap="gray")
+            plt.title("Image")
 
-    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
-    plt.show()
+            plt.subplot(3, 3, 2)
+            plt.imshow(np.squeeze(y_test[choose_rand]),cmap="gray")
+            plt.title("Mask")
 
-model.train_model(filepath=filepath, X_train=X_train, y_train=y_train, 
-                  X_val=X_val, y_val=y_val, epochs=20, display_callback=DisplayCallback())
+            plt.subplot(3, 3, 3)
+            plt.imshow(np.squeeze(train_model.predict(prediction)),cmap="gray")
+            plt.title("Predicted Mask")
 
-train_model.evaluate(X_test,y_test)
+            plt.axis('off')
+
+            print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
+            plt.show()
+
+    model.train_model(filepath=filepath, X_train=X_train, y_train=y_train, 
+                    X_val=X_val, y_val=y_val, epochs=20, display_callback=DisplayCallback())
+
+    train_model.evaluate(X_test,y_test)
