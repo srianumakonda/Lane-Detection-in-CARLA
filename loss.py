b8e1f8a0d4ac16158a5d3ea8feda4a013309bb01
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-01-16 20:30:13 -0500

    Create loss.py

---
+++ loss.py
@@ -0,0 +1,23 @@
+from keras import backend as K
+# All credit goes to https://github.com/nabsabraham/focal-tversky-unet/blob/master/losses.py for the loss functions
+
+smooth=1
+def tversky(y_true, y_pred):
+    y_true_pos = K.flatten(y_true)
+    y_pred_pos = K.flatten(y_pred)
+    true_pos = K.sum(y_true_pos * y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1-y_pred_pos))
+    false_pos = K.sum((1-y_true_pos)*y_pred_pos)
+    alpha = 0.7
+    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+def focal_tversky(y_true,y_pred):
+    pt_1 = tversky(y_true, y_pred)
+    gamma = 0.75
+    return K.pow((1-pt_1), gamma)
+def dice(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
