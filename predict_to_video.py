ef9466f5ebdeb73e57b9f3760877923ed27e1364
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-01-18 11:29:04 -0500

    Update preprocess_data.py

--- predict_to_video.py
+++ predict_to_video.py
@@ -1,31 +1,41 @@
-import loss
-import tensorflow as tf
 import cv2
 import os
 import numpy as np
-from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+from PIL import Image
+from preprocess_data import SplitData
 
 if __name__ == "__main__":
+    pred_img_path = "predictions"
+    filename_pred = []
+    train_set = SplitData.non_aug_data("X_train")
+    framesize = (128,128)
 
-    loaded_model = tf.keras.models.load_model("final_model.h5",
-                            custom_objects={'focal_tversky':loss.focal_tversky,'dice':loss.dice})
+    if int(np.array(train_set).ravel().shape[0])//128//128 == len(os.listdir(pred_img_path)):
 
-    img_list = []
-    pred_list = []
-    filepath = "carla-dataset\\train"
+        for i in os.listdir(pred_img_path):
+            filename_pred.append(i) 
 
-    for filename in os.listdir(filepath):
-        img = cv2.imread(os.path.join(filepath,filename))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img,(128,128))
-        img_list.append(img)
-        pred_list.append(np.squeeze(loaded_model.predict(np.array(img).reshape(1,128,128,1))))
-    img_list = np.asarray(img_list)
-    pred_list = np.asarray(pred_list)
-    plt.imshow(img_list[-1])
-    plt.imshow(pred_list[-1])
-    plt.show()
+        out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, framesize, False)
 
+        for i in range(len(filename_pred)):
+            img_act = train_set[i][:, :, ::-1].copy() 
+            img_act = np.squeeze(img_act)
+            img_act *= 255
+            img_act = np.uint8(img_act)
+
+            
+            img_pred = cv2.imread(os.path.join(pred_img_path,filename_pred[i]))
+            img_pred = cv2.cvtColor(img_pred, cv2.COLOR_BGR2GRAY)
+            img_pred = cv2.resize(img_pred,(128,128))
+            img_pred *= 255
+            img_pred = np.uint8(img_pred)
+
+            merge_img = cv2.addWeighted(img_pred,0.6,img_act,0.4,0)
+            out.write(merge_img)
+            
+    else:
+        raise RuntimeError("Length of train images is not the same as prediction images.")
+    out.release()
+    print("Finished writing video.")
