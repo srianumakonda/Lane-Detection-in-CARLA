b6a89274a4b774fc13386453414384342a479a70
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-01-18 18:01:28 -0500

    Update predict_to_video.py

--- predict_to_video.py
+++ predict_to_video.py
@@ -4,20 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
-from preprocess_data import SplitData
+from preprocess_data import non_aug_data
 
 if __name__ == "__main__":
     pred_img_path = "predictions"
     filename_pred = []
-    train_set = SplitData.non_aug_data("X_train")
+    train_set = non_aug_data("X_train",".\\carla-dataset\\train")
+    train_set = train_set.reshape(2922,128,128,1)
     framesize = (128,128)
 
-    if int(np.array(train_set).ravel().shape[0])//128//128 == len(os.listdir(pred_img_path)):
+    # for idx, filename in enumerate(os.listdir(pred_img_path)):
+    #     if idx%8 != 0:
+    #         os.remove(os.path.join(pred_img_path,filename))
+
+    if int(train_set.ravel().shape[0])//128//128 == len(os.listdir(pred_img_path)):
+
+        print("Successfully deleted unecessary files")
 
         for i in os.listdir(pred_img_path):
             filename_pred.append(i) 
 
-        out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, framesize, False)
+        out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 1, framesize, False)
 
         for i in range(len(filename_pred)):
             img_act = train_set[i][:, :, ::-1].copy() 
@@ -34,7 +41,7 @@ if __name__ == "__main__":
 
             merge_img = cv2.addWeighted(img_pred,0.6,img_act,0.4,0)
             out.write(merge_img)
-            
+                    
     else:
         raise RuntimeError("Length of train images is not the same as prediction images.")
     out.release()
